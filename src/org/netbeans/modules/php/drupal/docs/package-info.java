8bb0eec5dd33c80154e45927ee157109cc79491e
cklein <trancesilken@gmail.com>
2016-10-21 23:37:51 +0200

    fix #45
    - refactor built in documentation

--- src/org/netbeans/modules/php/drupal/docs/package-info.java
+++ src/org/netbeans/modules/php/drupal/docs/package-info.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates and open the template in
  * the editor.
  */
-@HelpSetRegistration(helpSet = "drupaldevel-hs.xml", position = 3205)
+@HelpSetRegistration(helpSet = "helpset.xml", position = 3205)
 package org.netbeans.modules.php.drupal.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
